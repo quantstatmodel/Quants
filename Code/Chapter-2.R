3b500adb60ea8646bc05e2c4ea494f4a594dca8e
Arun Saha <arunkumarsaha80@gmail.com>
2020-12-20 04:31:42 +0530

    Some change

--- Code/Chapter-2.R
+++ Code/Chapter-2.R
@@ -5,21 +5,39 @@ Source = "/Users/arunchaitali/Dropbox/ARUN & CHAITALI/Quant-Understanding/Data"
 library(caret)
 library(dplyr)
 library(vars)
-library(OIsurv)  ### For Survival Analysis
+library(smbinning)
 
 {	### Page : 51
-	Get_Data =  read.csv(paste(Source, "/chap2oneypd.csv", sep = ""))
+	Get_Data =  read.csv(paste(Source, "/chap2oneypd.csv", sep = "")) %>%
+				mutate_at(., vars(contains('date')), funs(as.Date)) %>%
+				mutate(max_arrears_12m = round(max_arrears_12m, 4), arrears_months = round(arrears_months, 4)) %>%
+				mutate(default_event = if_else(arrears_event == 1 | term_expiry_event == 1 | bankrupt_event == 1, 
+												1,
+												0)) %>% ### Define the Default
+				mutate(default_flag = dplyr::if_else(default_event == 1, 0, 1))
 		print(head(Get_Data))
 
-	Data = dplyr::mutate_at(Get_Data, vars(contains('date')), funs((as.Date(., format = '%Y-%m-%d'))))
-		print(head(Data))
-		### 1) 'tob'                   : Number of Qs Since Origination
-		### 2) 'start_time / end_time' : Represent : The window to include into ***CPH function*** to fit the model
+	Train_Index = caret::createDataPartition(Get_Data$default_event, p = .7, list = FALSE)
+		Train_Data = Get_Data[Train_Index, ]
+		Test_Data  = Get_Data[-Train_Index, ]
+}
 
-	### This is ***NOT used*** ANYWHERE Here
-	Default_Rate_Q = Data %>%
-					dplyr::group_by(report_date, year) %>%
-					dplyr::summarise(dr_QoQ = mean(default_flag)) %>%
-					dplyr::select(report_date, year, dr_QoQ)
-		print(head(Default_Rate_Q))
+{	### Page : 52
+	InformationValue_Analysis = smbinning.sumiv(df = Train_Data, y = "default_flag")
+		# It gives the user the ability to calculate, in one step, the IV for each characteristic of the dataset.
+
+		# http://r-statistics.co/Information-Value-With-R.html 
+
+		# Information Value (IV) is a measure of the [[predictive capability of a categorical x]] variable to accurately predict the goods and bads. 
+		# For each category of [[x]], information value is computed as:
+
+		# 	IV = ([[%]] good of all goods−perc bad of all bads) * WOE
+		# 	WOE = log([[%]] good of all goods / [[%]] bad of all bads)
+
+		# 	The total IV of a variable is the sum of IV’s of its categories. Here is what the values of IV mean according to Siddiqi (2006):
+
+		# 		Less than 0.02, then the predictor is not useful for modeling (separating the Goods from the Bads)
+		# 		0.02 to 0.1, then the predictor has only a weak relationship.
+		# 		0.1 to 0.3, then the predictor has a medium strength relationship.
+		# 		0.3 or higher, then the predictor has a strong relationship.
 }
