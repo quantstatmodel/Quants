4094c0bf73c44f7f8a925fe76e6b3675c62eb640
Arun Saha <arunkumarsaha80@gmail.com>
2020-12-21 00:28:04 +0530

    Some change

--- Code/Chapter-2.R
+++ Code/Chapter-2.R
@@ -6,6 +6,7 @@ library(caret)
 library(dplyr)
 library(vars)
 library(smbinning)
+library(LDPD) ### Low Default Probability
 
 {	### Page : 51
 	Get_Data =  read.csv(paste(Source, "/chap2oneypd.csv", sep = "")) %>%
@@ -24,7 +25,7 @@ library(smbinning)
 
 {	### Page : 52
 	InformationValue_Analysis = smbinning.sumiv(df = Train_Data, y = "default_flag")
-		# It gives the user the ability to calculate, in one step, the IV for each characteristic of the dataset.
+		# -->> It gives the user the ability to calculate, in one step, the IV for each characteristic of the dataset.
 
 		# http://r-statistics.co/Information-Value-With-R.html 
 
@@ -40,4 +41,20 @@ library(smbinning)
 		# 		0.02 to 0.1, then the predictor has only a weak relationship.
 		# 		0.1 to 0.3, then the predictor has a medium strength relationship.
 		# 		0.3 or higher, then the predictor has a strong relationship.
+}
+
+{	### Page : 80 | EXAMPLE 2.5.1
+	Portfolio_Data = data.frame(Rating = LETTERS[1:5], `Number of Accounts` = c(10, 40, 25, 15, 10), `Number of Defaults` = c(2, 1, 0, 0, 0), check.names = FALSE)
+		print(Portfolio_Data)
+
+	set.seed(123)
+
+	PD_1_Point = PTOnePeriodPD(Portfolio_Data[, 'Number of Accounts'], Portfolio_Data[, 'Number of Defaults'], conf.interval = 0.90)
+		### Pluto Tasche Approach
+		### VERY SENSITIVE to 'conf.interval'
+		print(PD_1_Point)
+
+	PD_1_Point_CAP_Nethod = VDBCalibratePD(portf.uncond = Portfolio_Data[, 'Number of Accounts'], pd.uncond.old = 0.03, pd.uncond.new = 0.03, AR = 0.90, 
+									rating.type = 'RATING')
+		print(PD_1_Point_CAP_Nethod)
 }
